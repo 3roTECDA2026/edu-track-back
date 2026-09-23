09210f574e0c4c5f4a84390631daa1f27ba0a254
danpinn24 <danapinna@gmail.com>
2026-09-23 15:37:44 -0300

    Borro

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ app.use('/api', routes);
 
 // Manejo de errores: siempre después de las rutas
 app.use(errorHandler);
->>>>>>> develop
+
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
